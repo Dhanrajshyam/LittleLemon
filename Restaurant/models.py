3492ba257310797dbca2fc571ce9bb64d6a05c39
Dhanrajshyam <shyamjck@gmail.com>
2025-01-31 11:08:13 +0530

    updated models Menu and Booking

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -6,8 +6,13 @@ class Booking(models.Model):
     no_of_guests = models.PositiveIntegerField(default=0)
     booking_date = models.DateTimeField()
     
+    def __str__(self):
+        return f'{self.name} | {self.booking_date.date()}'
 
 class Menu(models.Model):
     title = models.CharField(max_length=255, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     inventory =models.PositiveIntegerField(default=0)
+    
+    def __str__(self):
+        return f'{self.title} | stock {self.inventory}'    
