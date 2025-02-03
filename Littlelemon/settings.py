5d0014efc04f883e9899f06154a8bb4bb2934c51
Dhanrajshyam <shyamjck@gmail.com>
2025-02-03 14:58:22 +0530

    updated database host

--- Littlelemon/settings.py
+++ Littlelemon/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': os.getenv('MYSQL_DATABASE', 'littlelemon'),
         'USER': os.getenv('MYSQL_USER', 'admindjango'),
         'PASSWORD': os.getenv('MYSQL_PASSWORD', "admin"),
-        "HOST": os.getenv('MYSQL_HOST', 'localhost'),
+        "HOST": os.getenv('MYSQL_HOST', '127.0.0.1'),
         "PORT": os.getenv('MYSQL_PORT', "3306"),
         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
