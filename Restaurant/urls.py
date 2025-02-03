e2b557a62dc85e433c938bb14d5eaec541ba7531
Dhanrajshyam <shyamjck@gmail.com>
2025-02-03 12:01:06 +0530

    user management setup-partial done

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
+
+userrouter = DefaultRouter(trailing_slash=False)
+userrouter.register(r"users", UserViewSet, basename='user')
 
 urlpatterns = [
     path("",views.index, name="home"),
+    path("",include(userrouter.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
